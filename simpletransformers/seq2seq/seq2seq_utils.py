b619e79293d1d885a3b2f414312467ebdd3110ab
edanhauon <edan1346975>
2021-11-29 11:57:56 +0200

    revert loss

--- simpletransformers/seq2seq/seq2seq_utils.py
+++ simpletransformers/seq2seq/seq2seq_utils.py
@@ -849,28 +849,28 @@ class UnlikelihoodLoss:
         self.pos_token_id = pos_token_id
 
     def __call__(self, model, inputs, model_output):
-        # logits = model_output["logits"] if isinstance(model_output, dict) else model_output[0]
-        # labels = inputs.get("labels")
-        # sentence_labels = torch.tensor([label[0] for label in labels]).to(labels.device)
-        # labels[:,0] = 0
-        # labels = torch.roll(labels, -1, dims=1)
-        # labels[:,-1] = -100
-        # negatives = labels[sentence_labels == self.neg_tokn_id]
-        # positives = labels[sentence_labels == self.pos_token_id]
+        logits = model_output["logits"] if isinstance(model_output, dict) else model_output[0]
+        labels = inputs.get("labels")
+        sentence_labels = torch.tensor([label[0] for label in labels]).to(labels.device)
+        labels[:,0] = 0
+        labels = torch.roll(labels, -1, dims=1)
+        labels[:,-1] = -100
+        negatives = labels[sentence_labels == self.neg_tokn_id]
+        positives = labels[sentence_labels == self.pos_token_id]
 
-        # lprobs = torch.nn.functional.log_softmax(logits, dim=-1)
-        # negatives[negatives == -100] = 0
-        # negative_targets = torch.zeros_like(lprobs).scatter_(2, torch.unsqueeze(negatives, 0), 1)
-        # one_minus_probs = torch.clamp((1.0 - lprobs.exp()), min=1e-5)
-        # custom_loss = -torch.log(one_minus_probs)*negative_targets
-        # neg_loss = custom_loss.sum()
+        lprobs = torch.nn.functional.log_softmax(logits, dim=-1)
+        negatives[negatives == -100] = 0
+        negative_targets = torch.zeros_like(lprobs).scatter_(2, torch.unsqueeze(negatives, 0), 1)
+        one_minus_probs = torch.clamp((1.0 - lprobs.exp()), min=1e-5)
+        custom_loss = -torch.log(one_minus_probs)*negative_targets
+        neg_loss = custom_loss.sum()
         
-        # pos_inputs = {k:t[sentence_labels == self.pos_token_id] for k,t in inputs.items()}
-        # if positives.shape[0] > 0:
-        #     pos_outputs = model(**pos_inputs)
-        #     pos_loss = pos_outputs["loss"]
-        #     del pos_outputs
-        # else:
-        #     pos_loss = 0
-        # del sentence_labels, negatives, positives, labels, pos_inputs
-        return model_output["loss"]
+        pos_inputs = {k:t[sentence_labels == self.pos_token_id] for k,t in inputs.items()}
+        if positives.shape[0] > 0:
+            pos_outputs = model(**pos_inputs)
+            pos_loss = pos_outputs["loss"]
+            del pos_outputs
+        else:
+            pos_loss = 0
+        del sentence_labels, negatives, positives, labels, pos_inputs
+        return pos_loss + neg_loss
